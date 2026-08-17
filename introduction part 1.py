45674b6e973b7e5efd881ccaf2aed30837a48075
SASHANG RAJAGOPAL SRIPRAKASH <srisashang@gmail.com>
2026-08-17 19:42:41 +0530

    GITHUB INTRODUCTION PROBLEMS

---
+++ introduction part 1.py
@@ -0,0 +1,50 @@
+#HELLO WORLD
+if __name__ == '__main__':
+    print('Hello, World!')
+
+print('-------------------------------------------------------------------------------------------------------------------------------------------------------------')
+#ARETHAMETIC OPERATIONS
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a+b)
+    print(a-b)
+    print(a*b)
+
+print('-------------------------------------------------------------------------------------------------------------------------------------------------------------')
+#Add logic to print two lines. The first line should contain the result of integer division,  // . The second line should contain the result of float division,  / .
+#DIVISION
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a//b)
+    print(a/b)
+
+print('-------------------------------------------------------------------------------------------------------------------------------------------------------------')
+#IF ELSE
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    
+
+    n = int(input().strip())
+    if n % 2 != 0:
+        print("Weird")
+    elif n >= 2 and n <= 5:
+        print("Not Weird")
+    elif n >= 6 and n <= 20:
+        print("Weird")
+    else:
+        print("Not Weird")
+
+print('-------------------------------------------------------------------------------------------------------------------------------------------------------------')
+
+    
